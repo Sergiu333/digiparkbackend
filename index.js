2c9e2e514444fd778c57dc39bb8e17aed2583a74
Sergiu333 <anghelenicis59@gmail.com>
2024-12-20 00:14:44 +0200

    awsdasd

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ const PORT = process.env.PORT || 3000;
 
 
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, '0.0.0.0', () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+app.listen(3000, () => console.log("Server ready on port 3000."));
